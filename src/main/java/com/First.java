cf386dfd2732eb82b5a0a53ce279e5476a2c656b
clglyj@126 <1401309131@qq.com>
2020-04-18 08:08:10 +0800

    修改

--- src/main/java/com/First.java
+++ src/main/java/com/First.java
@@ -2,6 +2,5 @@ package com;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("--------------");
     }
 }
--- src/main/java/com/Second.java
+++ src/main/java/com/Second.java
@@ -1,6 +1,6 @@
 package com;
 
-public class First {
+public class Second {
     public static void main(String[] args) {
         System.out.println("--------------");
     }
